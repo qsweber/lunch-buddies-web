335143c5f40753417a9984018118f4271a87ac2a
quinn <quinn@quinnweber.com>
2020-04-04 15:25:35 -0700

    change copy

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 
 const Pricing: NextPage = () => (
   <Layout>
-    <h2>Try Free for 30 Days!</h2>
+    <h2>Try Lunch Buddies free for 30 Days!</h2>
     <p>After that, it's just $1 per user per month.</p>
     <p>
       On the first day of each month, you'll recieve an invoice for the previous
